f2c2bd3efcaa9a88b545295c5032b319cea33d7c
RohanDesai435 <111384848+RohanDesai435@users.noreply.github.com>
2022-08-19 11:13:26 -0700

    finished trap

---
+++ Unit 02/src/Trap.java
@@ -0,0 +1,15 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double areaTotal; 
+		areaTotal = 0.5 * ((double) height) * ((double) base1 + (double) base2); 
+		return (areaTotal);
+	}
+}
